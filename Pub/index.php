001953f58f76f9d30ab2a5672b35b7d83bc5e5b9
chana-chh <chanasrb@gmail.com>
2025-03-13 14:05:59 +0100

    del

--- Pub/index.php
+++ Pub/index.php
@@ -1,16 +1,3 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = AppFactory::create();
-
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Slim4 magacin!");
-    return $response;
-});
-
-$app->run();
+echo 'Hello world!';
