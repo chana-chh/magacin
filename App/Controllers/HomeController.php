39f191b48edd3d5c7dae5cdadb21774d4d7cb0f4
chana-chh <chanasrb@gmail.com>
2025-03-13 18:57:36 +0100

    twig

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -2,18 +2,11 @@
 
 namespace App\Controllers;
 
-use App\Classes\Auth;
-use App\Classes\Logger;
-use App\Models\Termin;
-use App\Models\Sala;
-use App\Models\TipDogadjaja;
-use App\Models\Podesavanje;
-
 class HomeController extends Controller
 {
     public function getHome($request, $response)
     {
-        dd("HomeController.getHome");
+        $this->flash->addMessage('success', 'Добродошли.');
         $this->render($response, 'home.twig');
     }
 }
